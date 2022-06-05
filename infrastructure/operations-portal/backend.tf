94a221eb87125e1d3ef0f369fd971818372227ec
fpereyrap <f.pereyrap@gmail.com>
2022-06-05 19:35:09 -0300

    go

--- infrastructure/operations-portal/backend.tf
+++ infrastructure/operations-portal/backend.tf
@@ -1,10 +1,1 @@
-terraform {
-  backend "s3" {
-    region         = "us-west-2"
-    bucket         = "terraform-state-storage-rfdk-sndbox"
-    key            = "operations-portal.tfstate"
-    dynamodb_table = "terraform-state-lock-rfdk-sndbox"
-    profile        = "fox-dev-sandbox"
-    encrypt        = true
-  }
-}
+#RANDOMSHIT
